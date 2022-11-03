4a6a3050adee59f0d0cdca989f06527acf57fd13
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2022-11-03 22:00:40 +0900

    Add illustrated test example

---
+++ npartitie/tests/README.md
@@ -0,0 +1,4 @@
+# Test illustrations
+
+### `test_cell.test_with_illustration`
+![img](./img/test_with_illustration.png)
--- npartitie/tests/test_cell.py
+++ npartitie/tests/test_cell.py
@@ -1,4 +1,6 @@
 from npartitie.core.Npartitie import Cell
+
+
 def test_has_boundary():
     # construct a cwc with redundant
     cell_0_0 = Cell(0, [])
@@ -27,9 +29,19 @@ def test_boundary_duplicated():
     cell_1_0 = Cell(1, [cell_0_0, cell_0_1])
     cell_1_1 = Cell(1, [cell_0_2, cell_0_1])
     cell_2_0 = Cell(2, [cell_0_0, cell_1_0])
-    cell_2_1 = Cell(2, [cell_0_0,cell_1_1])
+    cell_2_1 = Cell(2, [cell_0_0, cell_1_1])
     cell_3_0 = Cell(3, [cell_0_1, cell_2_1])
 
     assert cell_2_0.boundary_duplicated()
     assert not cell_2_1.boundary_duplicated()
     assert cell_3_0.boundary_duplicated()
+
+
+def test_with_illustration():
+    """
+    .. image:: img/test_with_illustration.png
+        :alt: code_quality
+    """
+    # TODO: implement test
+    # TODO: change test name to smth meaningful
+    # TODO (optional): add description
